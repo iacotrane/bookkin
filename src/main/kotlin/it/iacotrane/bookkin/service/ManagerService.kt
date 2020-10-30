e31a262235e8235dfa5406e2385fa92dc3cc4a15
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2020-10-30 15:05:58 +0100

    Added filter to make query for company fields and booking range more secure

--- src/main/kotlin/it/iacotrane/bookkin/service/ManagerService.kt
+++ src/main/kotlin/it/iacotrane/bookkin/service/ManagerService.kt
@@ -5,13 +5,19 @@ import it.iacotrane.bookkin.exception.CompanyNotFoundException
 import it.iacotrane.bookkin.exception.FieldNotFoundException
 import it.iacotrane.bookkin.mapper.BookingRangeMapper
 import it.iacotrane.bookkin.mapper.FieldMapper
+import it.iacotrane.bookkin.model.QBookingRange
+import it.iacotrane.bookkin.model.QField
 import it.iacotrane.bookkin.model.dto.BookingRangeDto
 import it.iacotrane.bookkin.model.dto.FieldDto
+import it.iacotrane.bookkin.model.dto.UserDto
 import it.iacotrane.bookkin.repository.BookingRangeRepository
 import it.iacotrane.bookkin.repository.CompanyRepository
 import it.iacotrane.bookkin.repository.FieldRepository
 import org.springframework.beans.BeanUtils
+import org.springframework.http.HttpStatus
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 
 @Service
 class ManagerService(
@@ -24,9 +30,11 @@ class ManagerService(
 
     fun addOrEditField(fieldDto: FieldDto, companyId: Long): FieldDto {
         val company = companyRepository.findById(companyId).orElseThrow { throw CompanyNotFoundException("Company with id: $companyId not found") }
+        checkPermission(companyId)
         val newField = fieldMapper.convertToEntity(fieldDto)
         return if (fieldDto.id != 0L) {
-            var olderField = fieldRepository.findById(fieldDto.id).orElseThrow { throw FieldNotFoundException("Field with id: ${fieldDto.id} not found. If you're trying to add a new field use id = 0") }
+            val fieldFilter = QField.field.company.id.eq(companyId).and(QField.field.id.eq(fieldDto.id))
+            var olderField = fieldRepository.findOne(fieldFilter).orElseThrow { throw FieldNotFoundException("Field with id: ${fieldDto.id} not found. If you're trying to add a new field use id = 0") }
             BeanUtils.copyProperties(newField, olderField)
             olderField = fieldRepository.save(olderField)
             fieldMapper.convertToDto(olderField)
@@ -38,17 +46,24 @@ class ManagerService(
     }
 
     fun addOrEditBookingRangeOfField(bookingRangeDto: BookingRangeDto, companyId: Long, fieldId: Long) {
-        val company = companyRepository.findById(companyId).orElseThrow { throw CompanyNotFoundException("Company with id: $companyId not found") }
+        checkPermission(companyId)
         val fieldEntity = fieldRepository.findById(fieldId).orElseThrow { throw FieldNotFoundException("Field with id: $fieldId not found") }
         val newBookingRange = bookingRangeMapper.convertToEntity(bookingRangeDto)
         if (bookingRangeDto.id != 0L) {
-            val olderBookingRange = bookingRangeRepository.findById(bookingRangeDto.id).orElseThrow { throw BookingRangeNotFoundException("Booking range with id: ${bookingRangeDto.id} not found. If you're trying to add a new booking range use id = 0") }
+            val bookingRangeField = QBookingRange.bookingRange.field.id.eq(fieldId).and(QBookingRange.bookingRange.field.company.id.eq(companyId))
+            val olderBookingRange = bookingRangeRepository.findOne(bookingRangeField).orElseThrow { throw BookingRangeNotFoundException("Booking range with id: ${bookingRangeDto.id} not found. If you're trying to add a new booking range use id = 0") }
             BeanUtils.copyProperties(newBookingRange, olderBookingRange)
             bookingRangeRepository.save(olderBookingRange)
         } else {
             fieldEntity.addBookingRange(newBookingRange)
             fieldRepository.save(fieldEntity)
         }
+    }
+
+    private fun checkPermission(companyId: Long) {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val userDto: UserDto = authentication.details as UserDto
+        if (userDto.companyId != companyId) throw HttpClientErrorException(HttpStatus.UNAUTHORIZED)
     }
 
 }
